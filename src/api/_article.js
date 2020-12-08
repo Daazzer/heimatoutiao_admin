6fc7dc84ef0f088d205a6f231d1d5b78fb31884b
Daazzer <Daazzer@github.com>
2020-12-08 16:09:10 +0800

    修改注释说明

--- src/api/_article.js
+++ src/api/_article.js
@@ -3,9 +3,9 @@ import axios from '@/utils/axios_http-config'
 /**
  * 文章列表
  * @param {Object} [params] 参数对象
- * @param {number} [data.category] 栏目id
- * @param {number} [data.pageIndex] 当前页码
- * @param {number} [data.pageSize] 每页显示数据条数
+ * @param {number} [params.category] 栏目id
+ * @param {number} [params.pageIndex] 当前页码
+ * @param {number} [params.pageSize] 每页显示数据条数
  * @returns {Promise<Response>}
  */
 const getArticle = params => axios.get('/post', { params }).then(res => [null, res]).catch(err => [err])
